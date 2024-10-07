2ff4d701eb6adc8423c32f52219399e671677bed
Stockvard <victor.e.axelsson@gmail.com>
2024-10-07 22:05:59 +0200

    Keeps spawning cows

--- game/game.gd
+++ game/game.gd
@@ -1,7 +1,8 @@
 extends Node2D
 const SETTINGS = preload("res://global/settings.tscn")
 @onready var settings_button: Button = $SettingsButton
-
+@onready var cow = preload("res://characters/devil_cow/devil_cow.tscn")
+var cowAlternator  = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	settings_button.pressed.connect(_settings_button_pressed)
@@ -10,3 +11,13 @@ func _ready() -> void:
 func _settings_button_pressed():
 	var settings_scene = SETTINGS.instantiate()
 	add_child(settings_scene)
+
+
+func _on_cow_spawner_timeout():
+	var cowEnemy = cow.instantiate()
+	if(cowAlternator%2 == 0):
+		cowEnemy.position = Vector2(randf_range(-20, 1000), -20)
+	else:
+		cowEnemy.position = Vector2(randf_range(-20, 1000), 1000)
+	cowAlternator = !cowAlternator
+	add_child(cowEnemy)
